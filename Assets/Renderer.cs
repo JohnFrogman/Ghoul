802856e683766af68474a28b20bc254862444180
Alex <Alex_R_HAyes@hotmail.co.uk>
2018-11-12 22:12:30 +0000

    Texturing for renderer

--- Assets/Renderer.cs
+++ Assets/Renderer.cs
@@ -5,25 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Renderer : MonoBehaviour {
     Tile[,] Grid;
-    int width = 3; int height = 3;
-    Vector3[] vertices;
+    int width = 50; int height = 50;
 
     void Awake()
     {
-        width = Random.Range(3, 20);
-        height = Random.Range(3, 20);
         Grid = new Tile[width, height];
         for (int i = 0; i < width; ++i)
             for (int j = 0; j < height; ++j)
                 Grid[i,j] = new Tile(new Vector2Int(i, j));
         foreach (Tile t in Grid)
             t.type = Random.value < 0.5 ? Tile.Types.Floor : Tile.Types.Wall;
-        //Grid[1,1].type = Tile.Types.Floor;
         Render();
     }
     private void Render()
     {
-        vertices = new Vector3[2 * (width + 1) * (height + 1)];
+        Vector3[] vertices = new Vector3[2 * (width + 1) * (height + 1)];
         Vector2[] uv = new Vector2[vertices.Length];
         for (int y = 0, i = 0; y < 2; y++)
         {
@@ -32,14 +28,18 @@ public class Renderer : MonoBehaviour {
                 for (int x = 0; x <= width; x++, i++)
                 {
                     vertices[i] = new Vector3(x, y, z);
-                    uv[i] = new Vector2((float)x / width, (float)y / height);
+                    uv[i] = new Vector2((float)x / width, (float)z / height);
                 }
             }
         }
         Mesh mesh = new Mesh();
         mesh.name = "MainMesh";
         mesh.vertices = vertices;
-        List<int> triangles = new List<int>();
+        mesh.uv = uv;
+
+        mesh.subMeshCount = 2;
+        List<int> floorTriangles = new List<int>();
+        List<int> wallTriangles = new List<int>();
         foreach (Tile t in Grid)
         {
             // index of all the vertices relevant to the tile t.
@@ -56,38 +56,39 @@ public class Renderer : MonoBehaviour {
                 int upperTopLeft = topLeft + (width + 1) * (height + 1);
                 int upperTopRight = topRight + (width + 1) * (height + 1);
 
-                triangles.AddRange(new int[] { upperBottomLeft ,upperTopLeft, upperBottomRight });
-                triangles.AddRange(new int[] { upperBottomRight ,upperTopLeft, upperTopRight });
+                wallTriangles.AddRange(new int[] { upperBottomLeft ,upperTopLeft, upperBottomRight });
+                wallTriangles.AddRange(new int[] { upperBottomRight ,upperTopLeft, upperTopRight });
 
                 //checking neighbours to see if we need to draw a wall. also draw a wall if it's the edge of the grid.
                 if (t.Position.x == 0 || Grid[t.Position.x - 1, t.Position.y].type == Tile.Types.Floor) // left neighbour
                 {
-                    triangles.AddRange(new int[] { upperTopLeft, upperBottomLeft, bottomLeft, });
-                    triangles.AddRange(new int[] { upperTopLeft, bottomLeft, topLeft });
+                    wallTriangles.AddRange(new int[] { upperTopLeft, upperBottomLeft, bottomLeft, });
+                    wallTriangles.AddRange(new int[] { upperTopLeft, bottomLeft, topLeft });
                 }
                 if (t.Position.x == width - 1 || Grid[t.Position.x + 1, t.Position.y].type == Tile.Types.Floor) // right neighbour
                 {
-                    triangles.AddRange(new int[] { upperTopRight, bottomRight, upperBottomRight });
-                    triangles.AddRange(new int[] { upperTopRight, topRight, bottomRight });
+                    wallTriangles.AddRange(new int[] { upperTopRight, bottomRight, upperBottomRight });
+                    wallTriangles.AddRange(new int[] { upperTopRight, topRight, bottomRight });
                 }
                 if (t.Position.y == height - 1 || Grid[t.Position.x, t.Position.y + 1].type == Tile.Types.Floor) // top neighbour
                 {
-                    triangles.AddRange(new int[] { upperTopLeft, topLeft, topRight });
-                    triangles.AddRange(new int[] { upperTopLeft, topRight, upperTopRight });
+                    wallTriangles.AddRange(new int[] { upperTopLeft, topLeft, topRight });
+                    wallTriangles.AddRange(new int[] { upperTopLeft, topRight, upperTopRight });
                 }
                 if (t.Position.y == 0 || Grid[t.Position.x, t.Position.y - 1].type == Tile.Types.Floor) // bottom neighbour
                 {
-                    triangles.AddRange(new int[] { upperBottomLeft, bottomRight, bottomLeft });
-                    triangles.AddRange(new int[] { upperBottomLeft, upperBottomRight, bottomRight });
+                    wallTriangles.AddRange(new int[] { upperBottomLeft, bottomRight, bottomLeft });
+                    wallTriangles.AddRange(new int[] { upperBottomLeft, upperBottomRight, bottomRight });
                 }
             }
             else
             {
-                triangles.AddRange(new int[] { bottomLeft, topLeft, bottomRight });
-                triangles.AddRange(new int[] { bottomRight, topLeft, topRight });
+                floorTriangles.AddRange(new int[] { bottomLeft, topLeft, bottomRight });
+                floorTriangles.AddRange(new int[] { bottomRight, topLeft, topRight });
             }
         }
-        mesh.triangles = triangles.ToArray();
+        mesh.SetTriangles(floorTriangles.ToArray(), 0);
+        mesh.SetTriangles(wallTriangles.ToArray(), 1);
         mesh.RecalculateNormals();
         GetComponent<MeshFilter>().mesh = mesh;
     }
